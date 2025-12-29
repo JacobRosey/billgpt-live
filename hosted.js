470d3f2f161e7f41ee2b0f0ff03c3d6b1e2007e2
Jacob Rosey <jrosey77@gmail.com>
2025-12-29 11:16:16 -0600

    use process.env.PORT

--- hosted.js
+++ hosted.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url';
 const { Pool } = pg
 const { getBillText, searchForBills } = legiscan;
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
