ed338cf469fc701bc1ab28b93584f1a0901f1852
Jacob Rosey <jrosey77@gmail.com>
2025-12-30 11:32:51 -0600

    search and sort bills

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ var activeMode = 'sort';
 
 var cachedBills = {}
 
-function sortBills(mode){
+window.sortBills = function(mode){
 
   if(mode == sortMode && activeMode == 'sort') return; //Clicked the already active button
   const button = document.getElementById(sortMode);
@@ -139,7 +139,7 @@ function renderCachedBills(arg){
   }
 }
 
-async function searchBills(){
+window.searchBills = async function(){
   const query = document.getElementById('search').value;
   const button = document.getElementById('search-button');
   button.classList.add('searching-button');
